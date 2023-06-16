190e3d83241c120411c858c120a8b8041df881e5
hiroki04y <hiroki02y104@icloud.com>
2023-06-16 16:16:15 +0900

    dockerfile編集

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-# ベースイメージ
-FROM node:18.16.0-alpine
-
-# ポートの開放
-EXPOSE 3000
-
-# コンテナプロセスの実行されるディレクトリの指定
-WORKDIR /usr/src/app
--- memory/src/MemList.jsx
+++ memory/src/MemList.jsx
@@ -1,6 +1,15 @@
 // コンポーネント
 import React from 'react'
 
+function formatDate(dateString) {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = ("0" + (date.getMonth() + 1)).slice(-2);
+    const day = ("0" + date.getDate()).slice(-2);
+
+    return `${year}-${month}-${day}`;
+}
+
 function MemList(props){
     const { mems, onDeleteMemory } = props;
 
@@ -13,13 +22,23 @@ function MemList(props){
             {mems.map((mem) => (
                 <div key={mem.id} className="mem-item">
                     <div class="mem-item-field">
-                        <div class="mem-title">{mem.name}</div><div class="mem-memo">{mem.memo}</div><div class="mem-genre">ジャンル:{mem.genre}</div>
-                        <br></br>
+                        <div className="mem-info">
+                            <div className="mem-genre">{mem.genre}</div>
+                            【<div className="mem-title">{mem.name}</div>】
+                            <div className="mem-date">
+                                {formatDate(mem.date)}
+                            </div>
+                        </div>
+                        <div class="mem-memo">{mem.memo}</div>
                         <div class="mem-right">
                             <button
+                            className="comp-button"
+                            onClick={() => handleDeleteClick(mem.id)}>
+                            完了
+                            </button>
+                            <button
                             className="delete-button"
-                            onClick={() => handleDeleteClick(mem.id)}
-                            >
+                            onClick={() => handleDeleteClick(mem.id)}>
                             削除
                             </button>
                         </div>
